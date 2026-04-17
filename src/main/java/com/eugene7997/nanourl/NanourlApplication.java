79c22a7f00d9366d3b133afee086ff64b93761b0
john <ypoh007@e.ntu.edu.sg>
2026-04-17 18:50:29 +0800

    Initial commit.
    Fresh from spring initializer.

---
+++ src/main/java/com/eugene7997/nanourl/NanourlApplication.java
@@ -0,0 +1,13 @@
+package com.eugene7997.nanourl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanourlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanourlApplication.class, args);
+	}
+
+}
